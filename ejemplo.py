a2f7ce5555b62fb2820f70355933b2df204c1d8d
Felipe Campusano <felipecampusano@proton.me>
2022-11-08 21:56:27 -0300

    Nuevos apuntes

---
+++ ejemplo.py
@@ -0,0 +1,18 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2 + 2
+4
+>>> 3 - 1
+2
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> cls
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+>>> print("Hola mundo");
+Hola mundo
